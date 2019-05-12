dbe63a5c2b26801cdc8a789963d930e56078ee48
MonsieurCaillou <50424910+MonsieurCaillou@users.noreply.github.com>
2019-05-12 22:50:02 +0200

    Create utils.php

---
+++ php/utils.php
@@ -0,0 +1,34 @@
+<?php
+	
+	function getpackage_description($name) {
+ 		$string = array_pop(explode($name, file_get_contents('stable/Packages')));
+ 		$lines = explode(PHP_EOL, $string);
+ 		foreach($lines as $line) {
+ 			if (strpos($line, 'Description') !== false) {
+ 				return str_replace("Description: ","",$line);
+ 				break;
+ 			}
+ 		}
+ 	}
+	
+	function getfiles($path, $extension) {
+		$files = array();
+		foreach (new DirectoryIterator($path) as $page) {
+			$ext = $page->getExtension();
+			$file = $page->getFilename();
+			if (!$page->isDot() && $ext == $extension) {
+				array_push( $files, $file );
+			}
+		}
+		return $files;
+	}
+	
+	function markdown($file) {
+		require_once('hb.parsedown.php');
+		$parsedown = new Parsedown();
+		$index= file_get_contents($file);
+		$html = $parsedown->text($index);
+		echo $html;
+	}
+	
+?>
